76a982977d9d10483db1dbac7906076253e02f16
unknown <charaka.20200241@iit.ac.lk>
2023-06-25 13:30:45 +0530

    server starts with db connection implemented

--- server.js
+++ server.js
@@ -1,1 +1,59 @@
-console.log('server started');
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
+
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to the databse
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: process.env.PG_HOST,
+    dialect: config.dialect,
+    pool: {
+      max: parseInt(process.env.PG_MAXCONN),
+      min: 0,
+      acquire: 60000,
+      idle: 10000,
+    },
+  }
+);
+sequelize
+  .authenticate()
+  .then(async () => {
+    console.log(
+      chalk.white.bgCyan.bold(`Connection has been established successfully.`)
+    );
+  })
+  .catch((error) => {
+    console.log(
+      chalk.white.bgRedBright.bold(
+        ` Unable to connect to the database: ${error.message} `
+      )
+    );
+    process.exit();
+  });
+
+// import routing files
+
+// set up routing paths
+
+// start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(
+    chalk.white.bgGreen.bold(' PORT ') +
+      chalk.white.bgBlue.bold(` ${PORT} `) +
+      chalk.white.bgGreen.bold(' MODE ') +
+      chalk.white.bgBlue.bold(` ${process.env.NODE_ENV} `)
+  );
+});
